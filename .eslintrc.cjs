1f824d8be42eb98c8e3503e317270af68edfcaac
James Kaguru Kihuha <47499826+James-Kaguru@users.noreply.github.com>
2024-02-08 08:33:40 +0300

    setup prettier plugin for tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'prettier'
+    'prettier',
+    'prettier-plugin-tailwindcss'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
